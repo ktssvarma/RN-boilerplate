5aeff29d7f8a40799698630b712e3db3059d34ee
Sashanka <sashanka@cryptoforce.in>
2023-10-14 23:30:20 +0530

    file Restructure

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Provider} from 'react-redux';
-import {store} from './redux/store';
-import ScreensContainer from './Screens';
+import {store} from './src/redux/store';
+import ScreensContainer from './src/Screens';
 
 const App = () => {
   return (
