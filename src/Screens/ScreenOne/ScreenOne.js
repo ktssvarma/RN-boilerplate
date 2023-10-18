707d9021aa97b6a69d5c8f036dfa5b50f0599b69
Sashanka <sashanka@cryptoforce.in>
2023-10-18 14:26:17 +0530

    Themes Added

--- src/Screens/ScreenOne/ScreenOne.js
+++ src/Screens/ScreenOne/ScreenOne.js
@@ -1,20 +1,25 @@
 import { StatusBar, Text, TouchableOpacity, View } from 'react-native'
 import React, { Component } from 'react'
 import { screenThreeRoute } from '../../constants';
+import ThemeContext from '../../Theme/ThemeContext';
 
 export class ScreenOne extends Component {
   render() {
     const {navigation} = this.props;
     return (
-      <View style={{paddingTop: 0}}>
-        <TouchableOpacity
-          style={{height: 40, backgroundColor: 'teal'}}
-          onPress={() => {
-            navigation.navigate(screenThreeRoute);
-          }}
-        />
-        <Text>ScreenOne</Text>
-      </View>
+      <ThemeContext.Consumer>
+        {({theme}) => (
+          <View style={{paddingTop: 0, flex: 1, backgroundColor: theme.background}}>
+            <TouchableOpacity
+              style={{height: 40, backgroundColor: 'teal'}}
+              onPress={() => {
+                navigation.navigate(screenThreeRoute);
+              }}
+            />
+            <Text>ScreenOne</Text>
+          </View>
+        )}
+      </ThemeContext.Consumer>
     );
   }
 }
